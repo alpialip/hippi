1d5cbe409af1aab9e73d6402bf3fe3e29d083519
termsconditions <rifqithomi@gmail.com>
2015-01-20 08:54:17 +0700

    dbcon
    change DB

--- venda/dbcon.php
+++ venda/dbcon.php
@@ -5,9 +5,9 @@
 * Linkedin : id.linkedin.com/in/rifqithomi/
 */
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'hippi');
-define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_NAME', 'hippicoi_hippiadmin');
+define('DB_USER','hippicoi_admin');
+define('DB_PASSWORD','H1ppiCoi?');
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 //$base_url='http://demobacakanbdg.esy.es/';
